fe8f3917e0548ce98f3ee3f8128e9f342fa50f97
kaushalprateek <pkausha4@my.centennialcollege.ca>
2019-03-28 17:07:46 -0400

    initial commit

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,37 +1,36 @@
-let express = require('express');
+let express = require("express");
 let router = express.Router();
 
-let indexController = require('../controllers/index');
+let indexController = require("../controllers/index");
 
 /* GET home page. */
-router.get('/', indexController.displayHomePage);
+///router.get('/', indexController.displayHomePage);
 
 /* GET about page. */
-router.get('/about', indexController.displayAboutPage);
+////router.get('/about', indexController.displayAboutPage);
 
 /* GET products page. */
-router.get('/products', indexController.displayProductsPage);
+//router.get('/products', indexController.displayProductsPage);
 
 /* GET services page. */
-router.get('/services', indexController.displayServicesPage);
+//router.get('/services', indexController.displayServicesPage);
 
 /* GET contact page. */
-router.get('/contact', indexController.displayContactPage);
+//router.get('/contact', indexController.displayContactPage);
 
 /* GET - displays the Login Page */
-router.get('/login', indexController.displayLoginPage);
+router.get("/login", indexController.displayLoginPage);
 
 /* POST - processes the Login Page */
-router.post('/login', indexController.processLoginPage);
+router.post("/login", indexController.processLoginPage);
 
 /* GET - displays the User Registration Page */
-router.get('/register', indexController.displayRegisterPage);
+router.get("/register", indexController.displayRegisterPage);
 
 /* POST - processes the User Registration Page */
-router.post('/register', indexController.processRegisterPage);
+router.post("/register", indexController.processRegisterPage);
 
 /* GET - perform user logout */
-router.get('/logout', indexController.performLogout);
-
+router.get("/logout", indexController.performLogout);
 
 module.exports = router;
