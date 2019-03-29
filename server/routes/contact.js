0792682d7601949d0b25c06ca6df26bc9c78f049
kaushalprateek <pkausha4@my.centennialcollege.ca>
2019-03-29 01:20:25 -0400

    Angular setup updated

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -1,6 +1,8 @@
 let express = require("express");
 let router = express.Router();
 
+let jwt = require("jsonwebtoken");
+
 let passport = require("passport");
 
 let contactController = require("../controllers/contact");
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,10 +4,10 @@ let router = express.Router();
 let indexController = require("../controllers/index");
 
 /* GET home page. */
-///router.get('/', indexController.displayHomePage);
+//router.get('/', indexController.displayHomePage);
 
 /* GET about page. */
-////router.get('/about', indexController.displayAboutPage);
+//router.get('/about', indexController.displayAboutPage);
 
 /* GET products page. */
 //router.get('/products', indexController.displayProductsPage);
@@ -19,13 +19,13 @@ let indexController = require("../controllers/index");
 //router.get('/contact', indexController.displayContactPage);
 
 /* GET - displays the Login Page */
-router.get("/login", indexController.displayLoginPage);
+//router.get('/login', indexController.displayLoginPage);
 
 /* POST - processes the Login Page */
 router.post("/login", indexController.processLoginPage);
 
 /* GET - displays the User Registration Page */
-router.get("/register", indexController.displayRegisterPage);
+//router.get('/register', indexController.displayRegisterPage);
 
 /* POST - processes the User Registration Page */
 router.post("/register", indexController.processRegisterPage);
