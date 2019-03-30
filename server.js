45b06d2f8a225a4462cec161fe00c2a3c927b8e2
kaushalprateek <pkausha4@my.centennialcollege.ca>
2019-03-30 17:41:02 -0400

    final

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
  */
 
 let app = require("./server/config/app");
-let debug = require("debug")("comp308-w2019-Assignment02:server");
+let debug = require("debug")("comp308-w2019-lesson3b:server");
 let http = require("http");
 
 /**
